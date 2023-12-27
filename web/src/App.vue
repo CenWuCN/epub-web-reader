941ab06249cc847d623c494f8ff15f744f8c382d
CenWuCN <34904285@qq.com>
2023-12-28 01:26:27 +0800

    增加跳转到次级目录的功能

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,18 +2,48 @@
 // import HelloWorld from './components/HelloWorld.vue'
 import JSZip from 'jszip'
 import ePub, { NavItem } from 'epubjs'
-import {ref} from 'vue'
+import { ref, nextTick } from 'vue'
 
-function GetTocList(toc:NavItem[], j:number):Array<string> {
+// // 创建一个 MutationObserver 实例
+// let observer = new MutationObserver(function(mutations) {
+//   mutations.forEach(function(mutation) {
+//     // 在这里处理元素创建的逻辑
+//     if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+//       // 处理新添加的元素
+//       console.log('新元素已创建:', mutation.addedNodes);
+//     }
+//   });
+// });
+
+// // 选择要观察的目标节点
+// let targetNode = document.body;
+
+// // 配置观察选项（此处配置为监视子节点的变化）
+// let config = { childList: true, subtree: true };
+
+// // 传入目标节点和观察选项开始观察
+// observer.observe(targetNode, config);
+
+interface TocItem {
+  id: string;
+  href: string;
+  label: string;
+}
+
+function GetTocList(toc: NavItem[], j: number): Array<TocItem> {
   let spaces: string = ""
-  let toclist: Array<string> =[]
-  for(let i:number = 0; i < j; i++){
-    spaces = spaces + "  "
+  let toclist: Array<TocItem> = []
+  for (let i: number = 0; i < j; i++) {
+    spaces = spaces + "    "
   }
 
-  toc.forEach((element:any) => {
-    toclist.push(spaces + element.label.trim())
-    if (element.subitems.length > 0){
+  toc.forEach((element: NavItem) => {
+    toclist.push({
+      id: element.id,
+      href: element.href,
+      label: spaces + element.label.trim()
+    })
+    if (element.subitems.length > 0) {
       toclist = toclist.concat(GetTocList(element.subitems, j + 1))
     }
   });
@@ -21,17 +51,48 @@ function GetTocList(toc:NavItem[], j:number):Array<string> {
 }
 
 let book = ePub("/doc/11.epub");
-let rendition = book.renderTo("area", { flow: "scrolled-doc", width: "800"});
+let rendition = book.renderTo("area", { flow: "scrolled-doc", width: "800" });
 let displayed = rendition.display();
 rendition.themes.font("微软雅黑")
 
-let toclist = ref(Array<string>)
-book.loaded.navigation.then((navi)=>{
+let toclist = ref(Array<TocItem>)
+
+
+book.loaded.navigation.then((navi) => {
   console.log(navi.toc)
   toclist.value = GetTocList(navi.toc, 0)
   console.log(toclist)
 })
 
+let reg = /#(.*)/
+
+
+async function JumpToToc(link: string): void {
+  console.log("点击目录")
+  rendition.display(link).then(()=>{
+    let tocInHtmlId = reg.exec(link)
+    console.log(tocInHtmlId[1])
+    console.log(typeof(tocInHtmlId[1]))
+    
+    // await nextTick()
+    // let el = document.getElementById(tocInHtmlId[1])
+    let iframes = document.getElementsByTagName("iframe")
+    console.log(iframes[0].id)
+    let innerDoc = iframes[0].contentDocument || iframes[0].contentWindow?.document
+    let el = innerDoc?.getElementById(tocInHtmlId[1])
+    // let el = document.querySelector(".chapterCaption")
+
+    // console.log(el)
+    // el.scrollIntoView({behavior:'smooth'})
+    var offsetTop = el.offsetTop;
+
+    // 滚动到目标元素的位置
+      console.log(offsetTop)
+      window.scrollTo(0, offsetTop);
+
+  })
+
+}
 
 </script>
 
@@ -47,10 +108,12 @@ book.loaded.navigation.then((navi)=>{
     </el-col>
     <el-col :span="4">
       <div class="right-space">
-        <div id="toc">
-          <ul>
-            <li v-for="item in toclist">{{ item }}</li>
-          </ul>
+        <div id="toc-container">
+          <div id="toc">
+            <ul>
+              <li v-for="item in toclist" @click="JumpToToc(item.href)">{{ item.label }}</li>
+            </ul>
+          </div>
         </div>
       </div>
     </el-col>
@@ -75,11 +138,17 @@ book.loaded.navigation.then((navi)=>{
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+#toc-container {
+  position: relative;
 }
 
 li {
@@ -88,4 +157,7 @@ li {
   white-space: pre;
 }
 
+ul {
+  height: 800px;
+}
 </style>
